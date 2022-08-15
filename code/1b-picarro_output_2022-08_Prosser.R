b618746621d5bcb3dbc759639b74e05b802f45f0
kaizadp <50244730+kaizadp@users.noreply.github.com>
2022-08-15 12:39:26 -0700

    add August Picarro data

--- code/1b-picarro_output_2022-08_Prosser.R
+++ code/1b-picarro_output_2022-08_Prosser.R
@@ -105,7 +105,7 @@ ghg_ppm_max %>%
   labs(title = "Prosser",
        y = "CO2, ppm",
        color = "column number",
-       caption = "preliminary graph, prepared 2022/08/10")+
+       caption = "preliminary graph, prepared 2022/08/15")+
   facet_wrap(~treatment)+
   theme(legend.position = "top")
 
@@ -144,12 +144,12 @@ ghg_fluxes %>%
   labs(title = "Beaver Creek",
        y = "CO2 flux, μmol/s",       
        color = "column number",
-       caption = "preliminary graph, prepared 2022/06/12")+
+       caption = "preliminary graph, prepared 2022/08/15")+
   theme(legend.position = "none")
 
 # 4. export data ----
-ghg_ppm_max %>% write.csv("data/processed/picarro_Prosser_Aug10_2022_ppm", row.names = FALSE)
-ghg_fluxes %>% write.csv("data/processed/picarro_Prosser_Aug10_2022_flux", row.names = FALSE)
+ghg_ppm_max %>% write.csv("data/processed/picarro_Prosser_Aug15_2022_ppm", row.names = FALSE)
+ghg_fluxes %>% write.csv("data/processed/picarro_Prosser_Aug15_2022_flux", row.names = FALSE)
 
 
 
