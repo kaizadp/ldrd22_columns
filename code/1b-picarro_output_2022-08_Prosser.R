163ac542697c3b0f71e66aab40f4069a6203d81e
kaizadp <50244730+kaizadp@users.noreply.github.com>
2022-08-20 10:38:20 -0700

    run Picarro script 8/20

--- code/1b-picarro_output_2022-08_Prosser.R
+++ code/1b-picarro_output_2022-08_Prosser.R
@@ -105,7 +105,7 @@ ghg_ppm_max %>%
   labs(title = "Prosser",
        y = "CO2, ppm",
        color = "column number",
-       caption = "preliminary graph, prepared 2022/08/15")+
+       caption = "preliminary graph, prepared 2022/08/20")+
   facet_wrap(~treatment)+
   theme(legend.position = "top")
 
@@ -141,15 +141,15 @@ ghg_fluxes %>%
   geom_point(aes(color = as.character(column_number)))+ 
   geom_smooth(aes(group = measurement_location))+
   facet_wrap(~treatment) +
-  labs(title = "Beaver Creek",
+  labs(title = "Prosser",
        y = "CO2 flux, μmol/s",       
        color = "column number",
-       caption = "preliminary graph, prepared 2022/08/15")+
+       caption = "preliminary graph, prepared 2022/08/20")+
   theme(legend.position = "none")
 
 # 4. export data ----
-ghg_ppm_max %>% write.csv("data/processed/picarro_Prosser_Aug15_2022_ppm", row.names = FALSE)
-ghg_fluxes %>% write.csv("data/processed/picarro_Prosser_Aug15_2022_flux", row.names = FALSE)
+ghg_ppm_max %>% write.csv("data/processed/picarro_Prosser_Aug20_2022_ppm.csv", row.names = FALSE)
+ghg_fluxes %>% write.csv("data/processed/picarro_Prosser_Aug20_2022_flux.csv", row.names = FALSE)
 
 
 
